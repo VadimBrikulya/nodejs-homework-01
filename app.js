851b7830b1e65929d238e7a30afffab3ea3d1543
Vadim Brikulya <im.master99@gmail.com>
2022-01-13 23:22:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const fs = require("fs/promises");
-fs.readFile("files/file.txt")
-    .then(data => console.log(data))
+
+fs.readFile("files/file.txt", "utf-8")
+    .then(data => {
+        console.log(data);
+        // const text = data.toString();
+        // console.log(text);
+    })
     .catch(error => console.log(error.message))
