96554e3d500418c312ee851e33bd28320e348cda
Vadim Brikulya <im.master99@gmail.com>
2022-01-15 15:36:57 +0200

    up

--- app.js
+++ app.js
@@ -7,9 +7,17 @@ const fileOperation = async(filePath, action = "read", data = "") => {
             const text = await fs.readFile(filePath, "utf-8");
             console.log(text);
             break;
+        case "add":
+            const result = await fs.appendFile(filePath, data);
+            // console.log(result);
+            break;
     }
 }
-fileOperation("files/file.txt");
+fileOperation("files/file.txt", "add", "\nтра та та ");
+
+
+
+
 // fs.readFile("files/file.txt", "utf-8")
 //     .then(data => {
 //         console.log(data);
