5b22f761e041d068e8ba5e3d298458863e444113
Vadim Brikulya <im.master99@gmail.com>
2022-01-15 15:28:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const fs = require("fs/promises");
 
-fs.readFile("files/file.txt", "utf-8")
-    .then(data => {
-        console.log(data);
-        // const text = data.toString();
-        // console.log(text);
-    })
-    .catch(error => console.log(error.message))
+
+const fileOperation = async(filePath, action = "read", data = "") => {
+    switch (action) {
+        case "read":
+            const text = await fs.readFile(filePath, "utf-8");
+            console.log(text);
+            break;
+    }
+}
+fileOperation("files/file.txt");
+// fs.readFile("files/file.txt", "utf-8")
+//     .then(data => {
+//         console.log(data);
+//         // const text = data.toString();
+//         // console.log(text);
+//     })
+//     .catch(error => console.log(error.message))
