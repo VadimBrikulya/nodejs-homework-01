62c2fd32ab862bd8f9042ac93efc0dfaaaeab0c0
Vadim Brikulya <im.master99@gmail.com>
2022-01-15 23:15:01 +0200

    up

--- app.js
+++ app.js
@@ -1,27 +1,8 @@
 const fs = require("fs/promises");
-
-
-const fileOperation = async(filePath, action = "read", data = "") => {
-    switch (action) {
-        case "read":
-            const text = await fs.readFile(filePath, "utf-8");
-            console.log(text);
-            break;
-        case "add":
-            const result = await fs.appendFile(filePath, data);
-            // console.log(result);
-            break;
-    }
-}
-fileOperation("files/file.txt", "add", "\nтра та та ");
+const path = require("path");
+const contactsPath = require("./db/contact.json"); 
 
 
 
 
-// fs.readFile("files/file.txt", "utf-8")
-//     .then(data => {
-//         console.log(data);
-//         // const text = data.toString();
-//         // console.log(text);
-//     })
-//     .catch(error => console.log(error.message))
+
