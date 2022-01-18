87d5bdd5480ee9a3bb33f39043911ad1a8f834a2
Vadim Brikulya <im.master99@gmail.com>
2022-01-18 19:44:05 +0200

    up

--- app.js
+++ app.js
@@ -11,11 +11,14 @@ const invokeAction = async ({ action, id, data }) => {
             break;
         case "getById":
             const contacts = await contactsPath.getById(id);
+            if (!contacts) {
+                throw new Error(`mazafaka id=${id} not found`);
+            }
             console.log(contacts);
             break;
         default:
-      console.warn('\x1B[31m Unknown action type!');
+      console.warn("\x1B[31m Unknown action type!");
     }
 }
-const id = "6";
+const id = "1";
 invokeAction({ action: "getById", id});
--- db/getById.js
+++ db/getById.js
@@ -3,6 +3,9 @@ const getAll = require("./getAll");
 const getById = async (id) => {
     const contact = await getAll();
     const result = contact.find(item => item.id === id);
+    if (!result) {
+        return null;
+    }
     return result;
 }
 module.exports = getById;
